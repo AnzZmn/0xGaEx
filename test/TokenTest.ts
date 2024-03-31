c50ed2332a0e829fa00a8aced550b77576bd0f34
AnzZmn <92879745+AnzZmn@users.noreply.github.com>
2024-03-31 14:59:16 -0400

    Update TokenTest.ts

--- test/TokenTest.ts
+++ test/TokenTest.ts
@@ -26,6 +26,15 @@ async function fixture() {
 describe("Decentralized Exchange Protocol Test Suite",()=>{
         
         describe('GToken Test Cases',()=>{
+
+            it('should mint the Token Properly from Acc1',async()=>{
+                const { tokenContract, acc1 , publicClient } = await loadFixture(fixture)
+                const minTx = await tokenContract.write.mint([acc1.account.address, TOKEN_ID, URI ,acc1.account.address,ROYALTY],{account: acc1.account.address})
+                const TxReciept = await publicClient.getTransactionReceipt({hash: minTx});
+                const ownerAddress = await tokenContract.read.ownerOf([1n])
+                expect(ownerAddress.toUpperCase()).to.eq(acc1.account.address.toUpperCase())
+            })
+
             it(`Should deploy with Name: ${NAME} , Symbol: ${SYMBOL} and BaseURL: ${BASE_URL} `, async ()=>{
                 const { tokenContract   } = await loadFixture(fixture)
                 const name = await tokenContract.read.name()
@@ -36,30 +45,15 @@ describe("Decentralized Exchange Protocol Test Suite",()=>{
                 expect(baseUri).to.eq(BASE_URL);
             })
     
-            it('Should mint the Token Properly from Acc1 To Acc1', async()=>{
+            it('Should Set The TokenURI upon minting new Token', async()=>{
                 const { tokenContract, acc1, publicClient   } = await loadFixture(fixture)
                 const minTx = await tokenContract.write.mint([acc1.account.address, TOKEN_ID, URI ,acc1.account.address,ROYALTY],{account: acc1.account.address})
-                const ownerAddress = await tokenContract.read.ownerOf([1n])
                 const tokenURI = await tokenContract.read.tokenURI([TOKEN_ID])
-                const TransactionReceipt = publicClient.getTransactionReceipt({hash: minTx})
-                const fromAddress = (await TransactionReceipt).from
-                expect(fromAddress.toUpperCase()).to.eq(ownerAddress.toUpperCase());
                 expect(tokenURI).to.eq(`MY_CUSTOM_URI${URI}`)
             })
     
-    
-            it('should mint the Token Properly from Acc1',async()=>{
-                const { tokenContract, acc1, acc2, publicClient   } = await loadFixture(fixture)
-                const minTx = await tokenContract.write.mint([acc1.account.address, TOKEN_ID, URI ,acc1.account.address,ROYALTY],{account: acc1.account.address})
-                const TxReciept = await publicClient.getTransactionReceipt({hash: minTx});
-                const ownerAddress = await tokenContract.read.ownerOf([1n])
-                expect(ownerAddress.toUpperCase()).to.eq(acc1.account.address.toUpperCase())
-
-            })
-    
-    
             it(`Should set the Royalty to ${ROYALTY}%`,async()=>{
-                const { tokenContract, acc1, acc2, publicClient } = await loadFixture(fixture)
+                const { tokenContract, acc1 } = await loadFixture(fixture)
                 const minTx = await tokenContract.write.mint([acc1.account.address, TOKEN_ID, URI ,acc1.account.address,ROYALTY],{account: acc1.account.address})
                 const [Recipeint, Royalty] = await tokenContract.read.royaltyInfo([TOKEN_ID,SALE_PRICE])
                 //console.log(`Royalty : ${formatEther(Royalty)}, recipient: ${Recipeint}, salePrice: ${formatEther(SALE_PRICE)}`);
@@ -72,7 +66,7 @@ describe("Decentralized Exchange Protocol Test Suite",()=>{
         describe('ExProtocol Test Cases',()=>{
 
             it('should Deploy Escrow Contract Properly',async()=>{
-                const { acc1, deployer, tokenContract,  acc2, publicClient, ApproveAcc, AdminContract } = await loadFixture(fixture)
+                const { acc1, deployer, tokenContract, AdminContract } = await loadFixture(fixture)
                 const minTx = await tokenContract.write.mint([acc1.account.address, TOKEN_ID, URI ,acc1.account.address,ROYALTY],{account: acc1.account.address})
                 const EscrowContract = await viem.deployContract("ExProtocol",[TOKEN_ID, SALE_PRICE , tokenContract.address, AdminContract.address],{value: SALE_PRICE})
                 const logs = (await EscrowContract.getEvents.contractDeployed())[0]["args"];
@@ -85,11 +79,11 @@ describe("Decentralized Exchange Protocol Test Suite",()=>{
             })
 
             describe('Should Transfer The Asset Properly', () => {
-                it('Should implement Approve function',async()=>{
-                    const { acc1, acc2, deployer , ApproveAcc , tokenContract, publicClient, AdminContract } = await loadFixture(fixture)
+                it('Should Set the Approver to Administrator Contract on Minting a New Token',async()=>{
+                    const { acc1, tokenContract, AdminContract } = await loadFixture(fixture)
                     const mintTx = await tokenContract.write.mint([acc1.account.address, TOKEN_ID, URI, acc1.account.address, ROYALTY],{account: acc1.account});
                     const approver = await tokenContract.read.getApproved([TOKEN_ID]);
-                    console.log(approver, AdminContract.address);
+                    expect(approver.toUpperCase()).to.equal(AdminContract.address.toUpperCase())
                 })
             })
 
